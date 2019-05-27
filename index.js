e6d1ab1520c81df8e671c4e55dc5f4e0e75dec7d
wideo2 <wideo2@naver.com>
2019-05-27 10:45:38 +0900

    웹팩 개념 설명

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import $ from 'jquery';
 
+import "bootstrap";
+
+import './app/mystyle.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 $(function(){
     $("#changeBtn").on("click", function(){
         $("h1").text("글자가 변경되었습니다. 변경변경");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
-
+const MiniCss = require("mini-css-extract-plugin");
 module.exports = {
     entry: './index.js',
     output: {
         filename:'app.js',
         path:path.resolve(__dirname, "dist")
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[{
+                    loader: MiniCss.loader,
+                    options:{
+                        hmr:process.env.NODE_ENV ==='development'
+                    }
+                },'css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new MiniCss({filename:"style.css"})
+    ],
+    resolve:{
+        modules :['node_modules'],
+        extensions: ['.js','.json','.jsx','.css']
     }
 }
